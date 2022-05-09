83e46b8b8b341c931418757686421fc4287c9fe2
Jake <jakespeitel19@gmail.com>
2022-05-09 18:13:33 -0400

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("fillerOne").style.left = "50%";
+document.getElementById("fillerTwo").style.left = "25%";
+document.getElementById("fillerThree").style.left = "75%";
+document.getElementById("fillerOne").style.top = "50%";
+var selectionDim = document.querySelector("#fillerOne");
+var currWidth = selectionDim.clientWidth;
+var posOne = document.getElementById("fillerOne").style.left;
+
+relocate();
+menu();
+window.addEventListener('resize', relocate);
+
+function relocate() {
+   
+    selectionDim = document.querySelector("#fillerOne");
+    currWidth = selectionDim.clientWidth;
+    currWidth = (currWidth/2);
+
+    document.getElementById("fillerOne").style.marginLeft= (`-${currWidth}px`);
+    document.getElementById("fillerTwo").style.marginLeft = (`-${currWidth}px`);
+    document.getElementById("fillerThree").style.marginLeft = (`-${currWidth}px`);
+}
+
+function menu() {
+    
+    document.getElementById("infoOne").appendChild("#fillerOne");
+
+}
